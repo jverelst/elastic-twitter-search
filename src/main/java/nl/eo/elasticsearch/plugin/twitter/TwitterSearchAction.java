86a251f8bfff5e47e195db8d8773a475162b992e
Johannes Verelst <git@filter.verelst.net>
2013-03-05 14:30:45 +0100

    implemented 'getTask' and 'listTasks'

--- src/main/java/nl/eo/elasticsearch/plugin/twitter/TwitterSearchAction.java
+++ src/main/java/nl/eo/elasticsearch/plugin/twitter/TwitterSearchAction.java
@@ -7,10 +7,13 @@ import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 
 import org.elasticsearch.client.Client;
 import org.elasticsearch.index.get.GetField;
 import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
@@ -29,7 +32,6 @@ import static org.elasticsearch.rest.action.support.RestXContentBuilder.restCont
 
 public class TwitterSearchAction extends BaseRestHandler {
 
-    public static String INDEX = "_twittersearch";
     public static String TYPE = "task";
 
     @Inject public TwitterSearchAction(Settings settings, Client client, RestController controller) {
@@ -38,101 +40,116 @@ public class TwitterSearchAction extends BaseRestHandler {
         // Define REST endpoints
 
         // Get a list of all tasks, mapped to 'listTasks'
-        controller.registerHandler(GET, "/_twittersearch/", this);
+        controller.registerHandler(GET, "/_twittersearch/{index}", this);
 
         // Get a specific task, mapped to 'getTask'
-        controller.registerHandler(GET, "/_twittersearch/{id}", this);
+        controller.registerHandler(GET, "/_twittersearch/{index}/{id}", this);
 
         // Delete a specific task, mapped to 'deleteTask'
-        controller.registerHandler(DELETE, "/_twittersearch/{id}", this);
+        controller.registerHandler(DELETE, "/_twittersearch/{index}/{id}", this);
 
         // Add a task, mapped to 'addTask'
-        controller.registerHandler(PUT, "/_twittersearch/", this);
+        controller.registerHandler(PUT, "/_twittersearch/{index}/", this);
 
         // Update a specific task
-        controller.registerHandler(PUT, "/_twittersearch/{id}", this);
+        controller.registerHandler(PUT, "/_twittersearch/{index}/{id}", this);
     }
 
-    private void deleteTask(final RestRequest request, final RestChannel channel, final String id) {
-
+    private void deleteTask(final RestRequest request, final RestChannel channel, final String index, final String id) {
+        // TODO:
+        // - Delete the task
+        // - Fetch all tasks, create river configuration
+        // - Re-initialize the river
     }
 
+    private void listTasks(final RestRequest request, final RestChannel channel, final String index) {
+        // TODO:
+        // - Fetch all tasks, return data
+        logger.info("listTasks()");
+        final SearchRequest searchRequest = new SearchRequest(index);
+        searchRequest.types(TYPE);
+        searchRequest.listenerThreaded(false);
 
-    private void listTasks(final RestRequest request, final RestChannel channel) {
+        SearchResponse searchResponse = client.prepareSearch(index).setTypes(TYPE).execute().actionGet();
+        try {
+            logger.info("Hits: " + searchResponse.hits().totalHits());
+            XContentBuilder builder = restContentBuilder(request);
+            builder.startObject();
+            searchResponse.toXContent(builder, request);
+            builder.endObject();
+            channel.sendResponse(new XContentRestResponse(request, OK, builder));
+        } catch (Throwable e) {
+            try {
+                e.printStackTrace();
+                channel.sendResponse(new XContentThrowableRestResponse(request, e));
+            } catch (IOException e1) {
+                logger.error("Failed to send failure response", e1);
+            }
+        }
 
+        logger.info("listTasks() finished");
     }
 
-    private void addTask(final RestRequest request, final RestChannel channel) {
-
+    private void addTask(final RestRequest request, final RestChannel channel, final String index) {
+        // TODO:
+        // - Add the task
+        // - Fetch all tasks, create river configuration
+        // - Re-initialize the river
+        // - Do a historic search
+        // - 
     }
 
-    private void updateTask(final RestRequest request, final RestChannel channel, final String id) {
-
+    private void updateTask(final RestRequest request, final RestChannel channel, final String index, final String id) {
+        // TODO:
+        // - Update the task
+        // - Fetch all tasks, create river configuration
+        // - Re-initialize the river
     }
     
 
-    private void getTask(final RestRequest request, final RestChannel channel, final String id) {
-        final GetRequest getRequest = new GetRequest(INDEX, TYPE, id);
-        getRequest.listenerThreaded(false);
-        getRequest.operationThreaded(true);
+    private void getTask(final RestRequest request, final RestChannel channel, final String index, final String id) {
+        GetResponse response = client.prepareGet(index, TYPE, id).setFields("keywords", "users", "follow", "ignore").execute().actionGet();
+        try {
+            logger.info("exists? " + response.exists());
+            XContentBuilder builder = restContentBuilder(request);
+            response.toXContent(builder, request);
 
-        String[] fields = {"keywords", "users"};
-        getRequest.fields(fields);
-
-        client.get(getRequest, new ActionListener<GetResponse>() {
-            @Override public void onResponse(GetResponse response) {
-
-                try {
-                    XContentBuilder builder = restContentBuilder(request);
-                    GetField field = response.field("msg");
-                    String greeting = (field!=null) ? (String)field.values().get(0) : "Sorry, do I know you?";
-                    builder
-                        .startObject()
-                        .field(new XContentBuilderString("hello"), id)
-                        .field(new XContentBuilderString("greeting"), greeting)
-                        .endObject();
-
-                    if (!response.exists()) {
-                        channel.sendResponse(new XContentRestResponse(request, NOT_FOUND, builder));
-                    } else {
-                        channel.sendResponse(new XContentRestResponse(request, OK, builder));
-                    }
-                } catch (Exception e) {
-                    onFailure(e);
-                }
+            if (!response.exists()) {
+                channel.sendResponse(new XContentRestResponse(request, NOT_FOUND, builder));
+            } else {
+                channel.sendResponse(new XContentRestResponse(request, OK, builder));
             }
-
-            @Override public void onFailure(Throwable e) {
-                try {
-                    channel.sendResponse(new XContentThrowableRestResponse(request, e));
-                } catch (IOException e1) {
-                    logger.error("Failed to send failure response", e1);
-                }
+        } catch (Exception e) {
+            try {
+                channel.sendResponse(new XContentThrowableRestResponse(request, e));
+            } catch (IOException e1) {
+                logger.error("Failed to send failure response", e1);
             }
-        });
-
+        }
     }
 
     public void handleRequest(final RestRequest request, final RestChannel channel) {
-        logger.debug("TwitterSearchAction.handleRequest called");
+        logger.info("TwitterSearchAction.handleRequest called");
+        final String index = request.hasParam("index") ? request.param("index") : "";
         final String id = request.hasParam("id") ? request.param("id") : "";
+        logger.info("Index: " + index + ", ID: " + id + ", method: " + request.method());
 
         switch(request.method()) {
             case DELETE:
-                deleteTask(request, channel, id);
+                deleteTask(request, channel, index, id);
                 break;
             case GET:
                 if ("".equals(id)) {
-                    listTasks(request, channel);
+                    listTasks(request, channel, index);
                 } else {
-                    getTask(request, channel, id);
+                    getTask(request, channel, index, id);
                 }
                 break;
             case PUT:
                 if ("".equals(id)) {
-                    addTask(request, channel);
+                    addTask(request, channel, index);
                 } else {
-                    updateTask(request, channel, id);
+                    updateTask(request, channel, index, id);
                 }
                 break;
         }
